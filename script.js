8532c619eca4c2f6220e55a98e831f847319f89c
Anndy Chael De La Cruz <58638309+chael88@users.noreply.github.com>
2022-12-24 16:46:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+            if ($(this).scrollTop() > 100) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+
+
+
+    //Portfolio-Section
+
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({
+            filter: $(this).data('filter')
+        });
+    });
+
+
+
+
+
+
+
+});
